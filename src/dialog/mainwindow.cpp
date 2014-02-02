cd138fd8f9fb2bab6f3e322f077930c39377f576
Takashiro <takashiro@qq.com>
2014-02-03 01:36:53 +0800

    Remove an unnecessary room
    Signed-off-by: Takashiro <takashiro@qq.com>

--- src/dialog/mainwindow.cpp
+++ src/dialog/mainwindow.cpp
@@ -166,7 +166,7 @@ void MainWindow::on_actionStart_Server_triggered()
 		return;
 
 	Server *server = new Server(this);
-	if(! server->listen()){
+	if(!server->listen()){
 		QMessageBox::warning(this, tr("Warning"), tr("Can not start server!"));
 
 		return;
@@ -611,8 +611,6 @@ void MainWindow::on_actionPC_Console_Start_triggered()
 
 		return;
 	}
-
-	server->createNewRoom();
 
 	Config.HostAddress = "127.0.0.1";
 	startConnection();
